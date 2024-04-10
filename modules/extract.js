e6f7c9fe6b8584c77741b38ec5e460cfb2afbc16
S200124 <s200124@studenti.polito.it>
2024-04-10 15:07:10 +0200

    Implemented deepDelete

--- modules/extract.js
+++ modules/extract.js
@@ -1,15 +1,28 @@
 import get from './get.js';
+import toPath from './toPath.js';
+import isEmpty from './isEmpty.js';
 import isUndefined from './isUndefined.js';
 
 /**
  * Extracts a value from an object and removes the corresponding key.
  * @param {Object} object - The input object.
  * @param {string} key - The key to extract.
+ * @param {boolean} deleteEmpty - If true, it deletes any empty object in the path (but not the root object).
  * @returns {*} The extracted value or undefined if the key is not found.
  */
-export default function extract(object, key) {
-  var value = get(object, key);
-  if(!isUndefined(value))
-	delete object[key];
-  return value;
+export default function extract(object, key, deleteEmpty) {
+	var deepDelete = function(object, keys, del) {
+		var key = keys.shift();
+		if(!key) return true;
+
+		if(deepDelete(object[key], keys, del)) {
+			delete object[key];
+			return isEmpty(object) && del;
+		}
+	}
+
+	var value = get(object, key);
+	if(!isUndefined(value))
+		deepDelete(object, toPath(key), Boolean(deleteEmpty));
+	return value;
 }
